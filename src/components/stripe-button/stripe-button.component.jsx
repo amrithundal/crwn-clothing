ffcecab92ce3e4f69dfcfc096bf6f9d4da67b9b1
Amritpal <amritpal.se@gmail.com>
2021-05-12 15:29:56 +0530

    Stripe Payment Added

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IqD0XSJHiuYScJmo0TEbKCComKCX7rsBl8vfomIa7iWy0oR4VGFbTt2WdWN7gH7nDwiWCOioI2pIC8lJRcOfqXP00f7prp6GL';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
